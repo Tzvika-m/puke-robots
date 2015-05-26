777d50f5560dac235749a2a371ab8724f5c01a8d
matan <matandavidcohen@gmail.com>
2015-05-26 09:47:02 -0700

    Adding the caplebility of readig from the config file , and init the variables.

--- HelloRobot/main.cpp
+++ HelloRobot/main.cpp
@@ -5,13 +5,77 @@
  *      Author: colman
  */
 
-#include <libplayerc++/playerc++.h>
 #include <iostream>
+#include <libplayerc++/playerc++.h>
+#include <fstream>
+using namespace std;
+
 
 using namespace PlayerCc;
 
+// Struct definition
+struct position
+{
+	int x;
+	int y;
+};
+
+struct size
+{
+	int width;
+	int height;
+};
+
+// Globals declaration
+const char* filePath = "Resources/config.txt";
+string mapFilePath = "";
+position startPostion;
+int startYaw;
+position destination;
+size robotSize;
+int mapResolution;
+int gridSize;
+
+void ReadConfigFile()
+{
+	ifstream configFile(filePath);
+	if(!configFile.is_open())
+	{
+		cout << " the file wasnt opened" << endl;
+	}
+	else
+	{
+		// Readign the map file
+		configFile >> mapFilePath;
+
+		// Start position
+		configFile >> startPostion.x;
+		configFile >> startPostion.y;
+		configFile >> startYaw;
+
+		// destination
+		configFile >> destination.x;
+		configFile >> destination.y;
+
+		// size
+		configFile >> robotSize.width;
+		configFile >> robotSize.height;
+
+		// Resolution
+		configFile >> mapResolution;
+
+		// grid size
+		configFile >> gridSize;
+
+		configFile.close();
+	}
+
+}
 
 int main() {
+
+	// Read the config file and init
+	ReadConfigFile();
 
 	PlayerClient pc("localhost", 6665);
 	SonarProxy lp(&pc);
